00828f2c49adb758c41feaacdc21809a2c7f00c1
mma1377 <alijani.mohammadmahdi@gmail.com>
2023-10-25 11:27:36 +0200

    Create UDP client project with Hello World

---
+++ UDPClient/src/UDPClient.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+
+public class UDPClient {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+    }
+}
