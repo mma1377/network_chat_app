09cf98475e3960d955dcb395307c4c2a00d0e98b
mma1377 <alijani.mohammadmahdi@gmail.com>
2023-10-25 10:47:36 +0200

    UDPServer class is implemneted

---
+++ UDP_Server/src/UDPServer.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.DatagramSocket;
+
+public class UDPServer {
+
+    public static void main(String[] args) throws IOException {
+        UDPServer udpServer;
+        if(args.length < 1)
+            udpServer = new UDPServer();
+        else {
+            udpServer = new UDPServer(Integer.parseInt(args[0]));
+        }
+        udpServer.launch();
+    }
+    private int _port;
+    private final String _host = "127.0.0.1";
+
+    UDPServer() {
+        _port = 8439;
+    }
+
+    UDPServer(int listening_port) {
+        _port = listening_port;
+    }
+
+    public void launch() throws IOException {
+        DatagramSocket socket = new DatagramSocket(_port);
+
+        socket.close();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
