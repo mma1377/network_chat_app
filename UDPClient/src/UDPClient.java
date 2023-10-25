9a54f1b7ac757feab185af099efab9c3455ceefe
maryshap <maryna.shapoval.uk@gmail.com>
2023-10-25 11:55:50 +0200

    Implimention UDP Client

--- UDPClient/src/UDPClient.java
+++ UDPClient/src/UDPClient.java
@@ -3,6 +3,31 @@ import java.net.DatagramSocket;
 
 public class UDPClient {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World");
+        UDPClient udpClient;
+        if (args.length < 1)
+            udpClient = new UDPClient();
+        else if (args.length < 2)
+            udpClient = new UDPClient(args[0]);
+        else {
+            udpClient = new UDPClient(args[0], Integer.parseInt(args[1]));
+        }
+        System.out.println(udpClient);
+    }
+
+    private int _port;
+    private String _host;
+    private DatagramSocket _socket;
+
+    UDPClient() {
+        _port = 8439;
+        _host = "localhost";
+    }
+    UDPClient(String host) {
+        _port = 8439;
+        _host = host;
+    }
+    UDPClient(String host, int port) {
+        _port = port;
+        _host = host;
     }
 }
