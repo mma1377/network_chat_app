669a873113f9d47aff1c681864bcf11225944eb4
mma1377 <alijani.mohammadmahdi@gmail.com>
2023-12-02 18:27:28 +0100

    small fixes

--- chat_app_project/src/com/ensea_chatapp_tcp/Server/ConnectionThread.java
+++ chat_app_project/src/com/ensea_chatapp_tcp/Server/ConnectionThread.java
@@ -43,6 +43,8 @@ public class ConnectionThread extends java.lang.Thread {
     public void run() {
         try {
             _socket = _tcpServer.GetServerSocket().accept();
+            System.out.println("A new connection is established with ip address " + _socket.getInetAddress().getHostAddress());
+
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
